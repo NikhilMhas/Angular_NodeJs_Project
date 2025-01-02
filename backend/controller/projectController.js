c84588acf0621700b9a7ccdadce3400332400484
Nikhil Mhaske <105561400+NikhilMhas@users.noreply.github.com>
2025-01-02 11:47:37 +0530

    Update projectController.js

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -170,10 +170,73 @@ const deleteProject = async (req, res, next) => {
     }
 };
 
+const getChartData = async (req, res, next) => {
+    try {
+        
+        const barChartData = [
+            { name: "Germany", value: 40632, extra: { code: "de" } },
+            { name: "United States", value: 50000, extra: { code: "us" } },
+            { name: "France", value: 36745, extra: { code: "fr" } },
+            { name: "United Kingdom", value: 36240, extra: { code: "uk" } },
+            { name: "Spain", value: 33000, extra: { code: "es" } },
+            { name: "Italy", value: 35800, extra: { code: "it" } }
+        ];
+
+        const heatmapData = [
+            {
+                name: "Germany",
+                series: [
+                    { name: "2010", value: 40632, extra: { code: "de" } },
+                    { name: "2000", value: 36953, extra: { code: "de" } },
+                    { name: "1990", value: 31476, extra: { code: "de" } }
+                ]
+            },
+            {
+                name: "United States",
+                series: [
+                    { name: "2010", value: 0, extra: { code: "us" } },
+                    { name: "2000", value: 45986, extra: { code: "us" } },
+                    { name: "1990", value: 37060, extra: { code: "us" } }
+                ]
+            },
+            {
+                name: "France",
+                series: [
+                    { name: "2010", value: 36745, extra: { code: "fr" } },
+                    { name: "2000", value: 34774, extra: { code: "fr" } },
+                    { name: "1990", value: 29476, extra: { code: "fr" } }
+                ]
+            },
+            {
+                name: "United Kingdom",
+                series: [
+                    { name: "2010", value: 36240, extra: { code: "uk" } },
+                    { name: "2000", value: 32543, extra: { code: "uk" } },
+                    { name: "1990", value: 26424, extra: { code: "uk" } }
+                ]
+            }
+        ];
+
+       
+        return res.json({
+            status: 'success',
+            data: { barChartData, heatmapData },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'An unexpected error occurred while fetching chart data.',
+        });
+    }
+};
+
+
 module.exports = {
     createProject,
     getAllProject,
     getProjectById,
     updateProject,
     deleteProject,
+    getChartData,
 };
